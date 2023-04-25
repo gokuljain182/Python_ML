3bc835d31d8584741e755c45023669febc2e5eae
gokuljain182 <gokuljain675@gmail.com>
2023-04-26 00:14:35 +0530

    Added Stream.py

--- Stream.py
+++ Stream.py
@@ -1,8 +1,10 @@
 import cv2
+
 vid = cv2.VideoCapture(0)
-while(True):
+while True:
     ref, frame = vid.read()
-    image = cv2.putText(frame,'Welcome to AI ML class', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    image = cv2.putText(frame, 'Welcome to AI ML class', (150, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 5,
+                        cv2.LINE_AA)
     cv2.imshow('Live Video', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         print("I am here")
