fabf13f285547a226ca605fc15998a5b81ba6e6d
gokuljain182 <gokuljain675@gmail.com>
2023-04-26 11:41:12 +0530

    Updated Patterns.py

--- Patterns.py
+++ Patterns.py
@@ -18,7 +18,40 @@
 #         print(j, end=" ")
 #     print()
 
-for i in range(1, 5):
-    for j in range(1, 4):
-        print(j + 4, end=" ")
+# for i in range(1, 5):
+#     for j in range(1, 4):
+#         print(j + 4, end=" ")
+#     print()
+
+# for i in range(1, 5):
+#     for j in range(1, i + 1):
+#         print(i, end=" ")
+#     print()
+#
+# for i in range(1, 5):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
+
+# for i in range(1, 5):
+#     for j in range(1, i + 1):
+#         print('*', end=" ")
+#     print()
+
+# for i in range(1, 5):
+#     for j in range(5 - i):
+#         print(i, end=" ")
+#     print()
+
+# for i in range(1, 5):
+#     for j in range(i, 5):
+#         print(i, end=" ")
+#     print()
+
+for i in range(1, 7):
+    for j in range(1, 7):
+        if i == j or i == 6 or j == 1:
+            print("*", end="  ")
+        else:
+            print(" ", end="  ")
     print()
