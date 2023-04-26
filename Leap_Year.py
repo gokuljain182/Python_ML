78b6dc3328d6e9ded46f7b0ad9f8456871e4a66d
gokuljain182 <gokuljain675@gmail.com>
2023-04-26 10:04:26 +0530

    Updated Leap_Year.py

--- Leap_Year.py
+++ Leap_Year.py
@@ -4,8 +4,8 @@ if n % 4 != 0:
 else:
     if n % 100 != 0:
         print("The year is a leap year")
-    elif n % 100 == 0 & n % 400 == 0:
-        print("The year is  a leap year")
-    elif n % 100 == 0 & n % 400 != 0:
-        print("This is not a leap year")
-
+    else:
+        if n % 400 != 0:
+            print("The year is not a leap year")
+        else:
+            print("The year is a leap year")
